f265970f5e9de5f7e49d4e816da86bc63a526927
Firas <ferasama@gmail.com>
2020-12-01 23:33:36 +0800

    reverse an integer

---
+++ src/practice/random/ReverseInteger.java
@@ -0,0 +1,18 @@
+package practice.random;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		
+		int input = 12345,
+		reverse = 0;
+		
+		while(input != 0) {
+			
+			reverse = reverse * 10 + (input % 10);
+			
+			input /= 10;
+		}
+		System.out.println(reverse);
+	}
+}
