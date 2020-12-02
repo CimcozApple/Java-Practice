77afaa9b2ce7e53346dfdffdbd130ea9968468cc
Firas <ferasama@gmail.com>
2020-12-02 08:54:46 +0800

    Count digits in a number

---
+++ src/practice/numbers/CountDigits.java
@@ -0,0 +1,33 @@
+package practice.numbers;
+
+public class CountDigits {
+	
+	public static void main(String[] args) {
+		
+	//count number of digits
+		
+		//1. convert to String then get the length
+		System.out.println("converting to String then geting the length");
+		int num = 24564532;
+		
+		//convert integer to String
+		String something = Integer.toString(num);
+		
+		//get the string's length
+		int length = something.length();
+		
+		System.out.println("number of digits in '" + num + "' = " + length);
+		
+		
+		//2. using while loop
+		System.out.println("\ncounting digits using while loop");
+		int count = 0;
+		
+		while(num > 0) {
+			num = num/10;
+			count++;
+		}
+		System.out.println("Number of digits = " + count);
+	}
+
+}
